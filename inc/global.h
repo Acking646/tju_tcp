c3e4cec063514094401402a9240aac1d2a607edc
Acking646 <3632176193@qq.com>
2025-09-18 17:35:31 +0800

    week1:pass

--- inc/global.h
+++ inc/global.h
@@ -16,8 +16,8 @@
 #include <sys/select.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "172.17.0.6"
-#define CLIENT_IP "172.17.0.5"
+#define SERVER_IP "172.17.0.3"
+#define CLIENT_IP "172.17.0.2"
 
 // 单位是byte
 #define SIZE32 4
